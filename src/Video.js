fcfeacdb6234b30e6626df62ba61bfeeab485d33
IsmaHaa12 <tutaven2@gmail.com>
2023-10-30 09:27:31 +0700

    change

--- src/Video.js
+++ src/Video.js
@@ -8,46 +8,46 @@ export default function Example()
         <Navbar />
         <div class="videos-container">
             <div class="video">
-                <iframe src="https://www.youtube.com/embed/dmYeJn5ae6o" width="420" height="240" frameborder="0" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/dmYeJn5ae6o" width="420" height="240" frameborder="0" allow="fullscreen"></iframe>
             </div>
             <div class="video">
-                <iframe src="https://www.youtube.com/embed/PvSPgwHLql4" width="420" height="240" frameborder="0" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/PvSPgwHLql4" width="420" height="240" frameborder="0" allow="fullscreen"></iframe>
             </div>
             <div class="video">
-                <iframe src="https://www.youtube.com/embed/6FUO3P-LGyw" width="420" height="240" frameborder="0" allowfullscreen></iframe>
-            </div>
-        </div>
-        <div class="videos-container">
-            <div class="video">
-                <iframe src="https://www.youtube.com/embed/DG0Dq8fYkf4" width="420" height="240" frameborder="0" allowfullscreen></iframe>
-            </div>
-            <div class="video">
-                <iframe src="https://www.youtube.com/embed/h0SC8YN0lOI" width="420" height="240" frameborder="0" allowfullscreen></iframe>
-            </div>
-            <div class="video">
-                <iframe src="https://www.youtube.com/embed/9qbTWZAFCHU" width="420" height="240" frameborder="0" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/6FUO3P-LGyw" width="420" height="240" frameborder="0" allow="fullscreen"></iframe>
             </div>
         </div>
         <div class="videos-container">
             <div class="video">
-                <iframe src="https://www.youtube.com/embed/DtBoAqkIJzI" width="420" height="240" frameborder="0" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/DG0Dq8fYkf4" width="420" height="240" frameborder="0" allow="fullscreen"></iframe>
             </div>
             <div class="video">
-                <iframe src="https://www.youtube.com/embed/8dywenCWPI0" width="420" height="240" frameborder="0" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/h0SC8YN0lOI" width="420" height="240" frameborder="0" allow="fullscreen"></iframe>
             </div>
             <div class="video">
-                <iframe src="https://www.youtube.com/embed/BEEFXAltoqo" width="420" height="240" frameborder="0" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/9qbTWZAFCHU" width="420" height="240" frameborder="0" allow="fullscreen"></iframe>
             </div>
         </div>
         <div class="videos-container">
             <div class="video">
-                <iframe src="https://www.youtube.com/embed/sgdPlDG1-8k" width="420" height="240" frameborder="0" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/DtBoAqkIJzI" width="420" height="240" frameborder="0" allow="fullscreen"></iframe>
             </div>
             <div class="video">
-                <iframe src="https://www.youtube.com/embed/w6G7Yfp1pVE" width="420" height="240" frameborder="0" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/8dywenCWPI0" width="420" height="240" frameborder="0" allow="fullscreen"></iframe>
             </div>
             <div class="video">
-                <iframe src="https://www.youtube.com/embed/1tk1pqwrOys" width="420" height="240" frameborder="0" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/BEEFXAltoqo" width="420" height="240" frameborder="0" allow="fullscreen"></iframe>
+            </div>
+        </div>
+        <div class="videos-container">
+            <div class="video">
+                <iframe src="https://www.youtube.com/embed/sgdPlDG1-8k" width="420" height="240" frameborder="0" allow="fullscreen"></iframe>
+            </div>
+            <div class="video">
+                <iframe src="https://www.youtube.com/embed/w6G7Yfp1pVE" width="420" height="240" frameborder="0" allow="fullscreen"></iframe>
+            </div>
+            <div class="video">
+                <iframe src="https://www.youtube.com/embed/1tk1pqwrOys" width="420" height="240" frameborder="0" allow="fullscreen"></iframe>
             </div>
         </div>
     </div>
