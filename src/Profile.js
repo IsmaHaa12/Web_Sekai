259933336c20850b26561c837a4a520d2e3fd115
IsmaHaa12 <tutaven2@gmail.com>
2023-11-03 22:05:30 +0700

    sekai

--- src/Profile.js
+++ src/Profile.js
@@ -12,7 +12,7 @@ function ProfilePage() {
 
   return (
     <div className="bg-cover bg-image-url-1 min-h-screen flex items-center justify-center" >
-      <div className="w-1/3 p-4 bg-white rounded-lg shadow-lg">
+      <div className="w-1/3 p-4 bg-cover bg-image-url-2 rounded-lg shadow-lg">
         <div style={customFont}>
           <img
             src="https://media.discordapp.net/attachments/1091296534370471997/1164390210080088114/K_1.png?ex=654309e3&is=653094e3&hm=5a3d33ebf1d16a4a78a189f598e3ec36b4cd5a8b66e792140f844c034e457335&=&width=741&height=741"
@@ -20,11 +20,11 @@ function ProfilePage() {
             className="w-48 h-48 mx-auto rounded-full mb-4"
           />
           <h1 style={{ ...customFont, ...boldText, fontSize: "1.5rem", color: "#333" }}>Isma Habiburrohman</h1>
-          <p style={{ ...customFont, fontSize: "1rem", color: "#666" }}>Mahasiswa Universitas Muhammadiyah Purwokerto</p>
+          <p style={{ ...customFont, ...boldText, fontSize: "1rem", color: "#333" }}>Mahasiswa Universitas Muhammadiyah Purwokerto</p>
         </div>
         <div className="mt-4">
-          <h2 style={{ ...customFont, fontSize: "1.25rem", color: "#333" }}>Kontak</h2>
-          <ul style={{ ...customFont, fontSize: "1rem", color: "#666" }} className="list-disc pl-6">
+          <h2 style={{ ...customFont, ...boldText, fontSize: "1.25rem", color: "#333" }}>Kontak</h2>
+          <ul style={{ ...customFont, ...boldText, fontSize: "1rem", color: "#333" }} className="list-disc pl-6">
             <p>Email: isma.haa.012@gmail.com</p>
             <p>Telepon: 081229211071</p>
             <p>Alamat: Pasir kec.Ayah kab.Kebumen</p>
